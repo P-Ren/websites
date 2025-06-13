6b30ebd83a434bb1d8767839f364ce79f9771595
Ren <phumren7515@gmail.com>
2025-06-13 16:34:49 +0700

    no message

--- index.php
+++ index.php
@@ -49,7 +49,7 @@
     <!-- menu section ends -->
 
     <!-- Review section starts -->
-   <?php include "newproduct.php"?>
+   
     <!-- Review section ends -->
 
     <!-- order section starts -->
@@ -61,9 +61,9 @@
     <!-- footer section ends -->
 
     <!-- loader part -->
-    <div class="loader-container">
+    <!-- <div class="loader-container">
         <img src="img/loading2.gif" alt="">
-    </div>
+    </div> -->
 
     <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
     <script src="js/js.js"></script>
--- newproduct.php
+++ newproduct.php
@@ -1,4 +1,84 @@
- <section class="review" id="review">
+ <!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>New Product</title>
+  <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css'>
+  <link rel="stylesheet" href="css/styleslide.css">
+
+</head>
+
+<body>
+  <!-- Swiper -->
+  <div class="swiper mySwiper" id="review">
+     <h3 class="sub-heading">New Products</h3>
+    <h1 class="heading"> what they say</h1>
+    <div class="swiper-wrapper">
+      <div class="swiper-slide">
+        <img
+          src="https://images.unsplash.com/photo-1432139555190-58524dae6a55?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=876&q=80" />
+        <div>
+          <h2>Grilled steak with vegetables</h2>
+        </div>
+      </div>
+      <div class="swiper-slide">
+        <img
+          src="https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=481&q=80" />
+        <div>
+          <h2>Pizza</h2>
+        </div>
+      </div>
+      <div class="swiper-slide">
+        <img
+          src="https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=410&q=80" />
+        <div>
+          <h2>Sandwich with boiled egg</h2>
+        </div>
+      </div>
+      <div class="swiper-slide">
+        <img
+          src="https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=449&q=80" />
+        <div>
+          <h2>Toast bread with blueberry</h2>
+        </div>
+      </div>
+      <div class="swiper-slide">
+        <img
+          src="https://images.unsplash.com/photo-1481070555726-e2fe8357725c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=435&q=80" />
+        <div>
+          <h2>Chicken burger</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src='https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js'></script>
+  <script>
+    var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: "1",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 4,
+    slideShadows: false
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2
+    }
+  }
+});
+  </script>
+
+</body>
+
+</html>
+ <!-- <section class="review" id="review">
         <h3 class="sub-heading">New Products</h3>
         <h1 class="heading"> what they say</h1>
 
@@ -89,4 +169,4 @@
 
             </div>
         </div>
-    </section>
+    </section> -->
