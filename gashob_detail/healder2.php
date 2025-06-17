525a5655208840ef59535bbb3485133846fd98a1
Ren <phumren7515@gmail.com>
2025-06-17 17:00:07 +0700

    no message

---
+++ gashob_detail/healder2.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light rounded-lg shadow-sm mb-5">
+        <div class="container-fluid">
+        
+                <img src="img/logo.svg" alt="Logo" class="d-inline-block align-text-top me-2 rounded"  style="width: 150px;"> 
+            
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item magin">
+                        <a class="nav-link active" aria-current="page" href="../index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link magin" href="#">Category</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link magin" href="#">Products</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link magin" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link magin" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
